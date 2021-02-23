e3484bc44d7eb0e806571c98da22f7ab88c0bcb2
Karlton Shoemaker <karltonshoemaker@gmail.com>
2021-02-23 12:04:19 -0500

    Added generic repository interface. Added MusicalRepository class. Loaded EntityFramework stuff. Created MusicalContext class to begin DB.

---
+++ ReviewsSite/Repository/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReviewsSite.Repository
+{
+    public interface IRepository<T> where T : class
+    {
+        void Create(T obj);
+
+        void Update(T obj);
+
+        IEnumerable<T> GetAll();
+
+        T GetByID(int id);
+
+        void Delete(T obj);
+        
+    }
+}
---
+++ ReviewsSite/Repository/MusicalRepository.cs
@@ -0,0 +1,42 @@
+﻿using ReviewsSite.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReviewsSite.Repository
+{
+    public class MusicalRepository : IRepository<Musical>
+    {
+        //public  Db<T> _db;
+
+        //void Create(T obj)
+        //{
+        //    _db.Add(obj);
+        //}
+        public void Create(Musical obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Musical obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Musical> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Musical GetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Musical obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
