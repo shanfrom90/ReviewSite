f4222b5bd7d5d979340f4f581cbdcc22ed9a0f28
marymo <mpkemp79@gmail.com>
2021-02-24 11:44:35 -0500

    added Review Model

--- ReviewsSite/Models/Musical.cs
+++ ReviewsSite/Models/Musical.cs
@@ -13,6 +13,8 @@ namespace ReviewsSite.Models
         public string Description { get; set; }
         public int Rating { get; set; }
         public string Comment { get; set; }
+        public int ReviewId { get; set; }
+        public Review Review { get; set; }
 
         public Musical()
         {
