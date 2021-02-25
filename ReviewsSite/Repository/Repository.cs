b19660d261066dde17ecdc4a34a7fe00f7cf901f
shanfrom90 <shanfrom90@gmail.com>
2021-02-25 11:07:56 -0500

    Added Generic Repository

---
+++ ReviewsSite/Repository/Repository.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ReviewsSite.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private MusicalContext _db;
+
+        public Repository(MusicalContext db)
+        {
+            this._db = db;
+        }
+        public void Create(T obj)
+        {
+            _db.Set<T>().Add(obj);
+            _db.SaveChanges();
+        }
+
+        public void Delete(T obj)
+        {
+            _db.Set<T>().Remove(obj);
+            _db.SaveChanges();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _db.Set<T>().ToList();
+        }
+
+        public T GetByID(int id)
+        {
+            return _db.Set<T>().Find(id);
+        }
+
+        public void Update(T obj)
+        {
+            _db.Set<T>().Update(obj);
+            _db.SaveChanges();
+
+        }
+    }
+}
